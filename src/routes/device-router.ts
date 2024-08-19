b7f857e5a88f29b9e9b7de175112bbcc51e97f5d
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-08-19 10:43:51 +0500

    refactor devices router

--- src/routes/device-router.ts
+++ src/routes/device-router.ts
@@ -1,31 +1,20 @@
-import {Router, Response, Request} from "express";
+import {Request, Response, Router} from "express";
 import {authMiddlewareRefresh} from "../middlewares/auth/auth-middleware";
 import {refreshTokenExpiration} from "../application/jwt-service"
 import {QuerySessionRepository} from "../repositories/query-session-repository";
-
 
 
 export const deviceRouter = Router({})
 
 deviceRouter.get('/devices', authMiddlewareRefresh, async (req: Request, res: Response)=>{
    const userId = req.userDto._id.toString()
-    const liveTime = refreshTokenExpiration //время жизни рефрештокена в секундах
-    // Получаем текущую дату и время
-    const now = new Date();
-   try {
-       const sessions = await QuerySessionRepository.getActiveDevises(userId, liveTime)
+    try {
+       const sessions = await QuerySessionRepository.getActiveDevises(userId, refreshTokenExpiration)
        res.status(200).send(sessions)
    }catch (error){
        res.status(500).send({error:'something goes wrong'})
    }
 
-// Вычисляем новое время, отнимая 20 секунд
-//     const newTime = new Date(now.getTime() - liveTime * 1000);
-//     const sessions = await sessionCollection.find(({
-//         userId: userId,
-//         lastActiveDate: { $gt: newTime }
-//     })).toArray()
-//     res.status(200).send(sessions)
 })
 
 deviceRouter.delete('/devices', authMiddlewareRefresh, async (req:Request, res:Response)=>{
